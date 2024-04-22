16395c919bf1263c2d2af38bc2428b443a387d2b
Nguyễn Minh Đức <148557640+nmd1406@users.noreply.github.com>
2024-04-23 00:42:22 +0700

    commit

--- lib/widgets/files_grid_view.dart
+++ lib/widgets/files_grid_view.dart
@@ -1,7 +1,6 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:notes/models/note.dart';
 import 'package:notes/providers/notes_provider.dart';
 import 'package:notes/widgets/file_grid_view_item.dart';
 
