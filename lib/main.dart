7afdefae48e78d85a91aa07514f061a30169047e
Nguyễn Minh Đức <mduc14062003@gmail.com>
2024-10-07 10:57:23 +0700

    setup workmanager

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,30 @@ import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:notes/screens/main_content.dart';
+import 'package:notes/services/database_service.dart';
+import 'package:workmanager/workmanager.dart';
+
+final _databaseService = DatabaseService.instance;
+
+void callbackDispatcher() {
+  Workmanager().executeTask(
+    (taskName, inputData) async {
+      _databaseService.monthlyCleanUpNotes();
+      return Future.value(true);
+    },
+  );
+}
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+  Workmanager().registerPeriodicTask(
+    "1",
+    "Monthly Clean Up Notes",
+    frequency: const Duration(days: 30),
+  );
+
   runApp(
     ProviderScope(
       child: Phoenix(
